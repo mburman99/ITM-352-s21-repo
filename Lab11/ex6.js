a7852e04769a175c21bd7decd42f1aa32aec4fdc
mburman99 <mburman@hawaii.edu>
2021-03-22 14:27:34 -0400

    test

--- Lab11/ex6.js
+++ Lab11/ex6.js
@@ -1,3 +1,26 @@
+function isNonNegInt(string_check, returnErrors = false) {
+    errors = []; // assume no errors at first
+    if(Number(string_check) != string_check) errors.push('Not a number!'); // Check if string is a number value
+    if(string_check < 0) errors.push('Negative value!'); // Check if it is non-negative
+    if(parseInt(string_check) != string_check) errors.push('Not an integer!'); // Check that it is an integer
+    return returnErrors ? errors: (errors.length==0);
+    }
+
+age = "21"
+name = "Max"
+
+parts = attributes.split(';')
+attributes = name + ";" + age + ";" + (age +0.5) + ";" + (age - 0.5);
+pieces = attributes.split(';');
+for(i in parts){
+    parts[i] = `${typeof parts[i]}${parts[i]}`;
+}
+
 function checkIt(part, i) { 
     console.log(`part ${index} is ${(isNonNegInt(item)?'a':'not a')} quantity`);
 }
+
+pices.forEach((item,index)=>{
+    console.log(`part ${index} is ${isNonNegInt(item)? 'a':'not a')} quantity`)
+})
+console.log(parts.join(","))
---
+++ WODs/Invoice/products.js
@@ -0,0 +1,28 @@
+var products = [
+    {
+        "Brand" : "HTC",
+        "Price" : 40.00,
+        "Image" : "http://dport96.github.io/ITM352/morea/080.flow-control-II/HTC.jpg"
+    },
+    {
+        "Brand" : "Apple",
+        "Price" : 75.00,
+        "Image" : "http://dport96.github.io/ITM352/morea/080.flow-control-II/iphone-3gs.jpg"
+    },
+    {
+        "Brand" : "Nokia",
+        "Price" : 35.00,
+        "Image" : "http://dport96.github.io/ITM352/morea/080.flow-control-II/Nokia.jpg"
+    },
+    {
+        "Brand" : "Samsung",
+        "Price" : 45.00,
+        "Image" : "http://dport96.github.io/ITM352/morea/080.flow-control-II/Samsung.jpg"
+    },
+    {
+        "Brand" : "Blackberry",
+        "Price" : 10.00,
+        "Image" : "http://dport96.github.io/ITM352/morea/080.flow-control-II/Blackberry.jpg"
+    }
+
+];
